047104b5bf483b6f7a03ecba01fa2e1c668aa189
slashmywrist <144715979+slashmywrist@users.noreply.github.com>
2023-09-12 01:56:19 +0930

    Create messages.lua

---
+++ messages.lua
@@ -0,0 +1,17 @@
+local discord = ".gg/praying"
+local botchats = {
+    discord .. " // boost 4 wl | rot",
+    "DUDE STOP FLINGING ME ðŸ˜­ðŸ˜­ðŸ˜­ | rot",
+    "before fling ðŸ˜ðŸ˜œ      after flingðŸ˜­ðŸ“¢ | rot",
+    "how does it feel to not have whitelist ðŸ¤£ðŸ¤£ðŸ¤£ðŸ˜‚ðŸ˜‚ðŸ˜¹ðŸ˜¹ðŸ˜¹ | rot",
+    "Mmmm i need CP, CP where? Pls CP me // " .. discord .. " | rot",
+    "god i wish i had a whitelist..... ðŸ™ðŸ™ðŸ¥ºðŸ¥ºðŸ¥º plzzzzz brooooo ðŸ› GIVE ITðŸ›ðŸ› // " .. discord .. " | rot",
+    "HOw do you get ACCESS to this Whitelist // " .. discord .. " | rot",
+    "plzzzz essex me ðŸ™ðŸ™ðŸ™ | rot",
+    "my anti fling is bypassing his bot lol ðŸ¤“ðŸ¤“ | rot",
+    "I'm based and you're cringe ðŸ˜­ðŸ¤“ | rot",
+    "I NEED A SCENEGIRL PLS HMU | rot",
+    "If u play destiny 2 HML add @ Lyoni on khord"
+}
+
+return botchats
