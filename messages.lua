6342aaf1339a0f03f24e205882a236922f2e8d01
slashmywrist <144715979+slashmywrist@users.noreply.github.com>
2023-09-13 17:29:57 -0400

    Update messages.lua

--- messages.lua
+++ messages.lua
@@ -9,7 +9,6 @@ local botchats = {
     "HOw do you get ACCESS to this Whitelist // " .. discord .. " | rot",
     "my anti fling is bypassing his bot lol 🤓🤓 | rot",
     "I'm based and you're cringe 😭🤓 | rot",
-    "I NEED A SCENEGIRL PLS HMU | rot",
     "HE WAS A SK8R BOI SHE SAID SEE YA LATER BOI | rot"
 }
 
