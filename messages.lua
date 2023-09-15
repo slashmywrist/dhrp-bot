d0176d983403055e73ae68e7a36587667b7150e9
slashmywrist <144715979+slashmywrist@users.noreply.github.com>
2023-09-15 15:16:55 -0400

    Update messages.lua

--- messages.lua
+++ messages.lua
@@ -2,14 +2,14 @@ local discord = ".gg/praying"
 local botchats = {
     discord .. " // boost 4 wl | rot",
     "DUDE STOP FLINGING ME 😭😭😭 | rot",
-    "before fling 😁😜      after fling😭📢 ".. discord .." | rot",
+    "before fling 😁😜      after fling😭📢 // " .. discord .. " | rot",
     "how does it feel to not have whitelist 🤣🤣🤣😂😂😹😹😹 | rot",
     "god i wish i had a whitelist..... 🙏🙏🥺🥺🥺 plzzzzz brooooo 🛐 GIVE IT🛐🛐 // " .. discord .. " | rot",
     "HOw do you get ACCESS to this Whitelist // " .. discord .. " | rot",
     "my anti fling is bypassing his bot lol 🤓🤓 | rot",
     "I'm based and you're cringe 😭🤓 | rot",
     "HE WAS A SK8R BOI SHE SAID SEE YA LATER BOI | rot",
-    "Krone - Im done flinging!11 TIme 2 StArt mY oWn Era, goes back 2 flinging 😂😂 // keep sending ur Pecker around 2 miners | rot"
+    "Krone - Im done flinging!11 TIme 2 StArt mY oWn Era, goes back 2 flinging 😂😂 // keep sending ur Pecker around 2 miners | rot",
 
 }
 
