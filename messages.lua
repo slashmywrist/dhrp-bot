767b6b11724cd9579e6dd4cdc40628c4ce86190c
slashmywrist <144715979+slashmywrist@users.noreply.github.com>
2023-09-21 22:38:30 -0400

    Update messages.lua

--- messages.lua
+++ messages.lua
@@ -1,4 +1,4 @@
-local discord = ".gg/praying"
+local discord = ".gg/m1v"
 local botchats = {
     discord .. " // boost 4 wl | rot",
     "DUDE STOP FLINGING ME 😭😭😭 | rot",
@@ -10,8 +10,8 @@ local botchats = {
     "I'm based and you're cringe 😭🤓 | rot",
     "HE WAS A SK8R BOI SHE SAID SEE YA LATER BOI | rot",
     "Krone - Im done flinging!11 TIme 2 StArt mY oWn Era, goes back 2 flinging 😂😂 // keep sending ur Pecker around 2 miners | rot",
-    ".gg/praying x .gg/dnr join up | rot",
-    ".gg/praying x .gg/dnr join up | rot",
+    ".gg/m1v x .gg/dnr join up | rot",
+    ".gg/m1v x .gg/dnr join up | rot",
 
 }
 
