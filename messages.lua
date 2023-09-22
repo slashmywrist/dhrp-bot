fa837c9312d29b38cf5c9ac1dbc6175fde1f099f
slashmywrist <144715979+slashmywrist@users.noreply.github.com>
2023-09-21 22:43:00 -0400

    Update messages.lua

--- messages.lua
+++ messages.lua
@@ -10,8 +10,8 @@ local botchats = {
     "I'm based and you're cringe 😭🤓 | rot",
     "HE WAS A SK8R BOI SHE SAID SEE YA LATER BOI | rot",
     "Krone - Im done flinging!11 TIme 2 StArt mY oWn Era, goes back 2 flinging 😂😂 // keep sending ur Pecker around 2 miners | rot",
-    ".gg/m1v x .gg/dnr join up | rot",
-    ".gg/m1v x .gg/dnr join up | rot",
+    ".gg/m1v x .gg/bnr join up | rot",
+    ".gg/m1v x .gg/bnr join up | rot",
 
 }
 
